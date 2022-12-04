f449f3ee1bd325aa6876efa713148a171fcab4f0
Vap0r1ze <superdash993@gmail.com>
2022-12-04 13:28:22 -0500

    fix: sanity

--- src/types/RemoteAuthClient.js
+++ src/types/RemoteAuthClient.js
@@ -3,6 +3,22 @@ const { magenta, blue, green, red } = require('colorette')
 const { EventEmitter } = require('eventemitter3')
 const WebSocket = require('ws')
 
+const COMMON_FETCH = {
+  headers: {
+    'accept-language': 'en-US,en;q=0.8',
+    'cache-control': 'no-cache',
+    'Content-Type': 'application/json',
+    'pragma': 'no-cache',
+    'referer': 'https://discord.com/login',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'sec-gpc': 1,
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
+  },
+  referrer: 'https://discord.com/login',
+}
+
 class RemoteAuthClient extends EventEmitter {
   constructor (options) {
     super()
@@ -94,60 +110,18 @@ class RemoteAuthClient extends EventEmitter {
         })
       break
       case 'pending_login':
-        if(!global.fetch) {
-          const fetch = require('node-fetch')
-          fetch("https://discord.com/api/v9/users/@me/remote-auth/login", {
-            body: JSON.stringify({ticket: p.ticket}),
-            headers: {
-              "accept-language": "en-US,en;q=0.8",
-              "cache-control": "no-cache",
-              'Content-Type': 'application/json',
-              "pragma": "no-cache",
-              "referer": "https://discord.com/login",
-              "sec-fetch-dest": "empty",
-              "sec-fetch-mode": "cors",
-              "sec-fetch-site": "same-origin",
-              "sec-gpc": 1,
-              "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
-            },
-            
-            method: "POST",
-            referrer: "https://discord.com/login",
-
+        const fetch = globalThis.fetch || require('node-fetch')
+        fetch('https://discord.com/api/v9/users/@me/remote-auth/login', {
+          body: JSON.stringify({ticket: p.ticket}),
+          method: 'POST',
+          ...COMMON_FETCH
         }).then(async response => {
-            const data = await response.json()
-            const decryptedToken = this.decryptPayload(data.encrypted_token).toString()
-            this.emit('finish', decryptedToken)
+          const data = await response.json()
+          const decryptedToken = this.decryptPayload(data.encrypted_token).toString()
+          this.emit('finish', decryptedToken)
         }).catch(e =>{
-            new Error("Failed to get token from remote auth",e.toString())
+          new Error('Failed to get token from remote auth', e.toString())
         })
-        } else {
-          fetch("https://discord.com/api/v9/users/@me/remote-auth/login", {
-            body: JSON.stringify({ticket: p.ticket}),
-            headers: {
-              "accept-language": "en-US,en;q=0.8",
-              "cache-control": "no-cache",
-              'Content-Type': 'application/json',
-              "pragma": "no-cache",
-              "referer": "https://discord.com/login",
-              "sec-fetch-dest": "empty",
-              "sec-fetch-mode": "cors",
-              "sec-fetch-site": "same-origin",
-              "sec-gpc": 1,
-              "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
-            },
-            
-            method: "POST",
-            referrer: "https://discord.com/login",
-
-        }).then(async response => {
-            const data = await response.json()
-            const decryptedToken = this.decryptPayload(data.encrypted_token).toString()
-            this.emit('finish', decryptedToken)
-        }).catch(e =>{
-            new Error("Failed to get token from remote auth",e.toString())
-        })
-        }
         // this.emit('finish', decryptedToken)
       break
       case 'cancel':
